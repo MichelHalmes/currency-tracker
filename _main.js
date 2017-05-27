3bf3a194ec2449c91f13a9a9f9749bb542a9b3e8
MichelHalmes <michel.halmes@takealot.com>
2017-05-27 20:01:52 +0200

    Get current rate

--- _main.js
+++ _main.js
@@ -1,12 +1,28 @@
-import SlackClient from './slack_client';
-
+import SlackClient from './slack_client'
+import rp from 'request-promise'
+import assert from 'assert'
 
 let slack = new SlackClient()
 
+const FINANCE_URL = 'https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json'
+rp(FINANCE_URL, {json: true})
+  .then(res => {
+    let resources = res.list.resources.map((rsrc) => rsrc.resource.fields)
+    resources = resources.filter((rsrc) => (rsrc.name == "USD/EUR" || rsrc.name == "USD/ZAR"))
 
-function sendNumber() {
-  slack.sendSlackMessage('aweee')
-  setTimeout(sendNumber, 1000);
+    assert(resources[0].name == "USD/ZAR")
+    let usd_zar = resources[0].price
 
-}
-sendNumber();
+    assert(resources[1].name == "USD/EUR")
+    let usd_eur = resources[1].price
+
+    let eur_zar = usd_zar/usd_eur
+
+    console.log("Got a res: ", eur_zar)
+    return eur_zar
+  })
+  .then(eur_zar => {
+    let slack = new SlackClient()
+    slack.sendSlackMessage(`Current rate is ${eur_zar.toFixed(2)} ZAR/EUR`)
+
+  })
